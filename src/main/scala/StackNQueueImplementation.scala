fc9fe1de65adf0720b4afe66932cbb7216a79f46
Yash Gupta <76771612+yashgupta99@users.noreply.github.com>
2021-02-28 23:55:45 +0530

    Create StackNQueueImplementation.scala

--- src/main/scala/StackNQueueImplementation.scala
+++ src/main/scala/StackNQueueImplementation.scala
@@ -79,39 +79,38 @@ object StackNQueueImplementation {
     val ob = new Stack()
     ob.push(25)
     ob.push(65)
-    ob.push(11);
-    ob.push(22);
-    ob.push(33);
-    ob.push(44);
+    ob.push(11)
+    ob.push(22)
+    ob.push(33)
+    ob.push(44)
 
     // print Stack elements
-    ob.display();
+    ob.display()
 
     // print Top element of Stack
-    println("\nTop element is ="+ob.peek());
+    println("\nTop element is ="+ob.peek())
 
     // Delete top element of Stack
-    ob.pop();
-    ob.pop();
+    ob.pop()
+    ob.pop()
 
     // print Stack elements
-    ob.display();
+    ob.display()
 
     // print Top element of Stack
-    println("\nTop element is ="+ob.peek());
+    println("\nTop element is ="+ob.peek())
 
 //Queue implementation
 
-    val q = new Queue();
-    q.enqueue(10);
-    q.enqueue(20);
-    q.dequeue();
-    q.dequeue();
-    q.enqueue(30);
-    q.enqueue(40);
-    q.enqueue(50);
-    q.dequeue();
-    q.list.
+    val q = new Queue()
+    q.enqueue(10)
+    q.enqueue(20)
+    q.dequeue()
+    q.dequeue()
+    q.enqueue(30)
+    q.enqueue(40)
+    q.enqueue(50)
+    q.dequeue()
 
 
 
